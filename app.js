33df97af0ddb4f968972624e54fb33243c703ef8
RickBodewes <Rick@planet.nl>
2020-09-13 17:04:54 +0200

    added basic viewable callendar and api to fetch data from database

--- app.js
+++ app.js
@@ -50,6 +50,14 @@ app.get('/calendar', (req, res) => {
     res.render('calendar', { title: 'calendar'});
 });
 
+// calendar events api
+app.get('/events/:year/:month', (req, res) => {
+    let query = `SELECT eventID, eventName, eventDescription, UNIX_TIMESTAMP(eventStartTime) as eventStartTime FROM events WHERE YEAR(eventStartTime) = ${con.escape(req.params.year)} AND MONTH(eventStartTime) = ${con.escape(req.params.month)}`;
+    con.query(query, (err, result) => {
+        res.send(JSON.stringify(result));
+    });
+});
+
 // 404 error when no file can be found
 app.use((req, res) => {
     res.status(404).render('status/404', { title: '404'});
--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,1 +1,46 @@
-//empty in here
+let today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+function RenderCalendar(month, year){
+    let firstDay = (new Date(year, month)).getDay();
+    let daysInMotnh = 32 - new Date(year, month, 32).getDate();
+
+    let calendar = document.getElementById('calendar-body');
+
+    //clearing the calendar
+    calendar.innerHTML = "";
+    let date = 1;
+    for (let i = 0; i < 6; i++){
+        let row = document.createElement('tr');
+
+        //creating dates
+        for (let j = 0; j < 7; j++){
+            if(i == 0 && j < firstDay){
+                let cell = document.createElement("td");
+                row.appendChild(cell);
+            }else if(date > daysInMotnh){
+                if(j > 0){
+                    let cell = document.createElement("td");
+                    row.appendChild(cell);
+                }else{
+                    break;
+                }
+            }else{
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode(date);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                date++;
+            }
+        }
+
+        calendar.appendChild(row);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    RenderCalendar(currentMonth, currentYear);
+});
